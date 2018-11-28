231630bf2d64ce0e2bf25f2640ff84aeecf70afe
Ruchika Mehta <ruchika.mehta@gomedia.io>
2018-11-28 10:00:49 +0000

    Assignment Part 2

--- PART2/helpers.js
+++ PART2/helpers.js
@@ -1,10 +1,7 @@
 function getPrefixedMessanger(text) {
-    
     function innerFunction() {
         const moreText = "moves the web forward!";
         return `${text} ${moreText}`
-       
     }
-
     return innerFunction;
 }
--- PART2/script.js
+++ PART2/script.js
@@ -2,7 +2,6 @@ var message;
 var prefix = "Javascript";
 var messanger = getPrefixedMessanger("Javascript");
 message = messanger();
-
 function showMessage(){
     document.getElementById("content").innerText = message;
 }
