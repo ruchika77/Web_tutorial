5058a7f16cd515883d94dd4ddbea60a9cdbad8e0
Ruchika Mehta <ruchika.mehta@gomedia.io>
2018-12-13 17:49:10 +0000

    Add Part 4

---
+++ PART4/README.md
@@ -0,0 +1,1 @@
+This part is about describing advanced function features - closure 
---
+++ PART4/script.js
@@ -0,0 +1,9 @@
+/*for (var i = 5; i >= 0; i--) {
+	console.log("Caution! You have " + i + " clicks of this button remaining")
+}
+*/
+var i;
+
+if ( i<= 5) {
+	console.log("Caution! You have " + i + " clicks of this button remaining")
+}
